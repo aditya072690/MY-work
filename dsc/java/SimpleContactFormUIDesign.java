188d5cce332cec51836435d0af09ae85e2968862
Aditya Chauhan <74345837+aditya072690@users.noreply.github.com>
2021-10-26 15:28:51 +0530

    Add files via upload

---
+++ dsc/java/SimpleContactFormUIDesign.java
@@ -0,0 +1,16 @@
+//Creating Simple User Forms User Interface (UI) Design in Android
+package viralandroid.com.androidxmluserinterfacetutorial;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
+
+public class SimpleContactFormUIDesign extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.simple_android_contact_form_ui_design);
+
+    }
+}
---
+++ dsc/java/t.java
@@ -0,0 +1,10 @@
+class t
+{
+    public static void main (String[] s)
+    {
+        int rate = 12;
+        double amt$ = 10000;
+        amt$ = rate * amt$;
+        System.out.println("Rate"+rate);
+    }
+}
